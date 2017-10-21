18d4beea3ae354281c30e784d9a95ca00c0afbc4
yudaiogawa <19630931+yudaiogawa@users.noreply.github.com>
2017-10-21 23:44:42 +0900

    Learned the super keyword.

---
+++ java/constructor/SuperClassConstructor.java
@@ -0,0 +1,32 @@
+class Super {
+
+  private String msg;
+  private int num;
+
+  Super(String msg, int num) {
+    this.msg = msg;
+    this.num = num;
+  }
+
+  void show() {
+    System.out.println(msg + " " + num);
+  }
+
+}
+
+class Sub extends Super {
+
+  Sub() {
+    super("キャッシュカードの暗証番号", 8931);
+  }
+
+}
+
+class SuperClassConstructor {
+
+  public static void main(String[] args) {
+    Sub sub = new Sub();
+    sub.show();
+  }
+
+}
