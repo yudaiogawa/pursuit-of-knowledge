69b96c78eb9744ba113ba4490c3840a0f409369b
yudaiogawa <19630931+yudaiogawa@users.noreply.github.com>
2017-10-29 02:48:07 +0900

    Added note of Git commands.

---
+++ git/commands.sh
@@ -0,0 +1,15 @@
+
+#### commit ################################################################
+
+# Commit Author 修正
+git commit --amend --author="username <account@domain.name>"
+
+#### log ###################################################################
+
+# Author と Commiter 確認
+git log --pretty=fuller
+
+#### filter-branch #########################################################
+
+# 全コミットの Author と Commiter 書き換える
+git filter-branch -f --env-filter "GIT_AUTHOR_NAME='username'; GIT_AUTHOR_EMAIL='email'; GIT_COMMITTER_NAME='username'; GIT_COMMITTER_EMAIL='email';" HEAD
