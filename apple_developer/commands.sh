f7ef8dd6c8b5def840c3a7b45d7cc8a7f7c5269b
yudaiogawa <19630931+yudaiogawa@users.noreply.github.com>
2017-11-09 22:29:25 +0900

    Create tips of commands & Added xcode-select.

---
+++ apple_developer/commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#### xcode-select ################################################################
+
+# 現在アクティブな developer directory 出力
+xcode-select --print-path
+
+# developer directory 切替 - 異なるバージョンの Xcode のツールを簡単に切り替えれる
+xcode-select --switch /Applications/Xcode0.0.0.app/Contents/Developer
