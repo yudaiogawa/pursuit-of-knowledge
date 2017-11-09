c44fd11a1e22473149385dbc075fe15daa66b844
yudaiogawa <19630931+yudaiogawa@users.noreply.github.com>
2017-11-09 23:10:47 +0900

    Update command of switching xcode tools.
    xcodeツールの切替は管理者権限じゃないと怒られました

--- apple_developer/commands.sh
+++ apple_developer/commands.sh
@@ -6,4 +6,4 @@
 xcode-select --print-path
 
 # developer directory 切替 - 異なるバージョンの Xcode のツールを簡単に切り替えれる
-xcode-select --switch /Applications/Xcode0.0.0.app/Contents/Developer
+sudo xcode-select --switch /Applications/Xcode0.0.0.app/Contents/Developer
