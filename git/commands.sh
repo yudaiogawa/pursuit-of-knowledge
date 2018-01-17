dfffdc328563ceb10427050476c7d8ca887de71d
Yudai Ogawa <19630931+yudaiogawa@users.noreply.github.com>
2018-01-17 19:02:16 +0900

    Add tips of git stash and cosmetic change.

--- git/commands.sh
+++ git/commands.sh
@@ -1,26 +1,31 @@
 
 #### commit ################################################################
 
-# Commit Author 修正
+# Modify the commit author.
 git commit --amend --author="username <account@domain.name>"
+
+#### stash #################################################################
+
+# Checking diff of stashed file.
+git stash show -p stash_name
 
 #### log ###################################################################
 
-# Author と Commiter 確認
+# Checking Author and Commiter.
 git log --pretty=fuller
 
 #### filter-branch #########################################################
 
-# 全コミットの Author と Commiter 書き換える
+# Rewrite author and commiter of all commits.
 git filter-branch -f --env-filter "GIT_AUTHOR_NAME='username'; GIT_AUTHOR_EMAIL='email'; GIT_COMMITTER_NAME='username'; GIT_COMMITTER_EMAIL='email';" HEAD
 
 #### cherry-pick ###########################################################
 
-# 特定のコミットだけを取り込めちゃう
+# Apply a specific commit.
 git cherry-pick SHA
 
 #### rebase ################################################################
 
-# 指定コミットにリベースできる
-# コミットメッセージ変更、コミットをまとめたりできる
+# Rebase a specific commit.
+# You can change the commit message and so on.
 git rebase -i SHA
