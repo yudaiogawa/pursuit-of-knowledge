ac4aa7965959c7e7981d860eb4199c557e9045cb
Yudai Ogawa <19630931+yudaiogawa@users.noreply.github.com>
2018-03-22 23:50:26 +0900

    Modify logic && Create method.

--- java/BitwiseOperator.java
+++ java/BitwiseOperator.java
@@ -1,14 +1,16 @@
 public class BitwiseOperator {
 
-  public static void main (String[] args) {
+  public static void main(String[] args) {
     int onSwitch  = 0b0000_0000_0000_0000_1000;
     int offSwitch = 0b0000_0000_0000_0000_0000;
-    int maskBit   = 0b0000_0000_0000_0000_1000;
 
-    int status = onSwitch & maskBit;
-    System.out.println("switch on : " + Integer.toHexString(status));
-    status = offSwitch & maskBit;
-    System.out.println("switch off : " + Integer.toHexString(status));
+    System.out.println("switch on number  : " + Integer.toHexString(logicalConjunction(onSwitch)));
+    System.out.println("switch off number : " + Integer.toHexString(logicalConjunction(offSwitch)));
+  }
+
+  public static int logicalConjunction(int bit) {
+    int mask = 0b0000_0000_0000_0000_1000;
+    return bit & mask;
   }
 
 }
