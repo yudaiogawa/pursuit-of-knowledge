5ae181dacfa94e78373e8d05972ef88b852c54ef
Yudai Ogawa <19630931+yudaiogawa@users.noreply.github.com>
2018-03-22 20:18:20 +0900

    Modify off switch && Add print number of switch off.

--- java/BitwiseOperator.java
+++ java/BitwiseOperator.java
@@ -1,10 +1,14 @@
 public class BitwiseOperator {
+
   public static void main (String[] args) {
     int onSwitch  = 0b0000_0000_0000_0000_1000;
-    int offSwitch = 0b0000_0000_0000_0000_1000;
+    int offSwitch = 0b0000_0000_0000_0000_0000;
     int maskBit   = 0b0000_0000_0000_0000_1000;
 
     int status = onSwitch & maskBit;
     System.out.println("switch on : " + Integer.toHexString(status));
+    status = offSwitch & maskBit;
+    System.out.println("switch off : " + Integer.toHexString(status));
   }
+
 }
