a29c15b79f30bbc1fdfff5c88216ef5423d26f14
Yudai Ogawa <19630931+yudaiogawa@users.noreply.github.com>
2019-04-17 02:56:27 +0900

    learn: Create the tcp client.

---
+++ cyber-security/create_tcp_client.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import socket
+
+host = "github.com"
+port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host,port))
+
+str = 'GET / HTTP/1.1\r\nHost: github.com\r\n\r\n'
+client.send(str)
+
+res = client.recv(4096)
+
+print res
