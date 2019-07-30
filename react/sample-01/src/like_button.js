b3513ab53df3230981d7473a0a7a119fab0421dd
Yudai Ogawa <19630931+yudaiogawa@users.noreply.github.com>
2019-07-31 01:33:24 +0900

    react: JSX && JSX Preprocesser

--- react/sample-01/src/like_button.js
+++ react/sample-01/src/like_button.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const e = React.createElement;
-
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
@@ -13,13 +11,13 @@ class LikeButton extends React.Component {
       return 'You liked this.';
     }
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
+    return (
+      <button onClick={() => this.setState({ liked: true })}>
+        Like
+      </button>
     );
   }
 }
 
 const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(<LikeButton />, domContainer);
