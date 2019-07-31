c3452edec2dfd260b7ce1de506a6f6837881d54b
Yudai Ogawa <19630931+yudaiogawa@users.noreply.github.com>
2019-08-01 00:17:48 +0900

    docker: run && stop the container

--- docker/hello-docker/main.go
+++ docker/hello-docker/main.go
@@ -9,10 +9,10 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("received request")
-		fmt.Fprintf(w, "Hello Docker!!")
+		fmt.Fprintf(w, "Hello Docker World!! ;)")
 	})
 
-	log.Println("start server")
+	log.Println("start server by Docker ;)")
 	server := &http.Server{Addr: ":8080"}
 	if err := server.ListenAndServe(); err != nil {
 		log.Println(err)
