189f57bbedd63d42648cfc365da9637c1fdeeb40
Yudai Ogawa <19630931+yudaiogawa@users.noreply.github.com>
2019-08-12 03:02:35 +0900

    python: learn basic

---
+++ python/intro/class01.py
@@ -0,0 +1,10 @@
+#! /usr/local/bin/python3
+
+print('load module...')
+
+def test():
+  print('function: Called test :)')
+
+if __name__ == '__main__':
+  print('python-intro')
+  test()
---
+++ python/intro/class02.py
@@ -0,0 +1,4 @@
+print("What's your name?")
+
+your_name = input('>>> ')
+print('Hello {} :)'.format(your_name))
