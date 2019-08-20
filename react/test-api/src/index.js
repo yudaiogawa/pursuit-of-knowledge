a70b57bf29631b1bf7effb1f3db56c9d5beb98d4
Yudai Ogawa <19630931+yudaiogawa@users.noreply.github.com>
2019-08-20 23:14:53 +0900

    react: Use local json-server and get api responce and display

--- react/test-api/src/index.js
+++ react/test-api/src/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const url = "http://localhost:8888/posts";
+
+class Posts extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: this.props.posts
+    };
+  }
+
+  render() {
+    return (
+      <section>
+        {this.state.posts.map(function(post, index) {
+          return (
+            <div key={post.id}>
+              <h1>{post.title}</h1>
+              <p>{post.text}</p>
+            </div>
+          )
+        }.bind(this))}
+      </section>
+    );
+  }
+}
+
+fetch(url).then(function(res) {
+  return res.json();
+}).then(function(json) {
+  ReactDOM.render(
+    <Posts posts={json} />,
+    document.getElementById('root')
+  );
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
