c9ba00e8f86a7a881cc8b6a2d107758011eea44f
Yudai Ogawa <19630931+yudaiogawa@users.noreply.github.com>
2019-08-22 00:43:32 +0900

    react: Display the api responce in ReactJS.

--- react/test-api/src/index.js
+++ react/test-api/src/index.js
@@ -5,6 +5,55 @@ import * as serviceWorker from './serviceWorker';
 
 const url = "http://localhost:8888/posts";
 
+class Form extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: "",
+      text: ""
+    }
+
+    this.changeTitle = this.changeTitle.bind(this);
+    this.changeText = this.changeText.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  changeTitle(event) {
+    this.setState({ title: event.target.value });
+  }
+
+  changeText(event) {
+    this.setState({ text: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.addContent(this.state.title, this.state.text);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="form">
+        <div>
+          <label>
+            <input type="text" placeholder="Title" value={this.state.title} onChange={this.changeTitle} />
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="text" placeholder="Text" value={this.state.text} onChange={this.changeText} />
+          </label>
+        </div>
+        <div>
+          <input type="submit" value="Add" />
+        </div>
+      </form>
+    );
+  }
+}
+
 class Posts extends React.Component {
 
   constructor(props) {
@@ -13,6 +62,26 @@ class Posts extends React.Component {
     this.state = {
       posts: this.props.posts
     };
+
+    this.addContent = this.addContent.bind(this);
+  }
+
+  addContent(title, text) {
+    fetch(url, {
+      method: 'post',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: 'title=' + title + "&text=" + text
+    }).then(function(res) {
+      return res.json();
+    }).then(function(json) {
+      this.state.posts.push({
+        id: json.id,
+        title: json.title,
+        text: json.text
+      });
+
+      this.setState(this.state);
+    }.bind(this));
   }
 
   render() {
@@ -20,12 +89,13 @@ class Posts extends React.Component {
       <section>
         {this.state.posts.map(function(post, index) {
           return (
-            <div key={post.id}>
+            <div key={post.id} className="content">
               <h1>{post.title}</h1>
               <p>{post.text}</p>
             </div>
           )
         }.bind(this))}
+        <Form addContent={this.addContent} />
       </section>
     );
   }
