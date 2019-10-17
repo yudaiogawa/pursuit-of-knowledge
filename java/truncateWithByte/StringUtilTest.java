e96eae932cb48f191f42dfad48cc9f2b8269adec
Yudai Ogawa <19630931+yudaiogawa@users.noreply.github.com>
2019-10-17 21:31:37 +0900

    java: Refactoring

--- java/truncateWithByte/StringUtilTest.java
+++ java/truncateWithByte/StringUtilTest.java
@@ -1,13 +1,48 @@
+import java.nio.charset.Charset;
+
 public class StringUtilTest {
 
   public static void main(String[] args) {
+    truncateWithByteTest();
     truncateWithByteSjisTest();
   }
 
-  public static void truncateWithByteSjisTest() {
-    String str = "AAAA,BBB,あ";
-    str = StringUtil.truncateWithByteSjis(str, 10);
+  public static void truncateWithByteTest() {
+    // Test1
+    Charset charset = Charset.forName("Shift_Jis");
+    String str = StringUtil.truncateWithByte(charset, "abcdef,ghi", 10);
+    String expected = "abcdef,ghi";
+    Boolean bool = str.equals(expected) ? true : false;
 
+    String methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+    System.out.println(methodName + ": Test1: " + bool);
+
+    // Test2
+    charset = Charset.forName("Shift_Jis");
+    str = StringUtil.truncateWithByte(charset, "abcdef,ghijklmn", 10);
+    bool = str.equals(expected) ? true : false;
+
+    System.out.println(methodName + ": Test2: " + bool);
+
+    // Test3
+    charset = Charset.forName("Shift_Jis");
+    str = StringUtil.truncateWithByte(charset, "abcdef,ghあijklmn", 10);
+    expected = "abcdef,gh";
+    bool = str.equals(expected) ? true : false;
+
+    System.out.println(methodName + ": Test3: " + bool);
+
+    // Test4
+    charset = Charset.forName("Shift_Jis");
+    str = StringUtil.truncateWithByte(charset, "abcdあef,ghiklmn", 10);
+    expected = "abcdあef,g";
+    bool = str.equals(expected) ? true : false;
+
+    System.out.println(methodName + ": Test4: " + bool);
+  }
+
+  public static void truncateWithByteSjisTest() {
+    String str = StringUtil.truncateWithByteSjis("AAAA,BBB,あ", 10);
     String expected = "AAAA,BBB,";
     Boolean bool = str.equals(expected) ? true : false;
 
