53215996588a4e7ae19989fc3101102dc4dbf2f1
Yudai Ogawa <19630931+yudaiogawa@users.noreply.github.com>
2019-10-18 03:49:42 +0900

    java: Refactoring

--- java/truncateWithByte/Main.java
+++ java/truncateWithByte/Main.java
@@ -1,5 +1,3 @@
-import java.nio.charset.Charset;
-
 public class Main {
 
   public static void main(String[] args) {
--- java/truncateWithByte/StringUtil.java
+++ java/truncateWithByte/StringUtil.java
@@ -10,19 +10,19 @@ public class StringUtil {
    * 指定の文字セットおよびバイト数以内に切り捨てた文字列を返します
    * @param str 文字列
    * @param charset 文字セット
-   * @param byteLength バイト数
+   * @param truncateByteSize バイト数
    * @return 文字列
    */
-  public static String truncateWithByte(Charset charset, String str, int byteLength) {
+  public static String truncateWithByte(Charset charset, String str, int truncateByteSize) {
     byte[] bytes = str.getBytes(charset);
 
-    int strByteLength = str.getBytes(charset).length;
-    if (strByteLength <= byteLength) {
+    int strByteSize = bytes.length;
+    if (strByteSize <= truncateByteSize) {
       return str;
     }
 
-    ByteBuffer byteBuf = ByteBuffer.wrap(bytes, 0, byteLength);
-    CharBuffer charBuf = CharBuffer.allocate(byteLength);
+    ByteBuffer byteBuf = ByteBuffer.wrap(bytes, 0, truncateByteSize);
+    CharBuffer charBuf = CharBuffer.allocate(truncateByteSize);
 
     CharsetDecoder decoder = charset.newDecoder();
     decoder.onMalformedInput(CodingErrorAction.IGNORE);
@@ -36,11 +36,11 @@ public class StringUtil {
    * 指定のバイト数以内に切り捨てた文字列を返します。
    * 文字セットはSJIS（全角1文字2byte）。
    * @param str 文字列
-   * @param byteLength バイト数
+   * @param truncateByteSize バイト数
    * @return 文字列
    */
-  public static String truncateWithByteSjis(String str, int byteLength) {
+  public static String truncateWithByteSjis(String str, int truncateByteSize) {
     Charset charset = Charset.forName("Shift_JIS");
-    return truncateWithByte(charset, str, byteLength);
+    return truncateWithByte(charset, str, truncateByteSize);
   }
 }
