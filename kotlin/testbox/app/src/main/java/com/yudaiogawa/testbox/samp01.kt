3285f3d43590e28ad746d39482b34cbc737aa588
yudaiogawa <19630931+yudaiogawa@users.noreply.github.com>
2020-09-04 00:01:33 +0900

    kotlin x junit

---
+++ kotlin/testbox/app/src/main/java/com/yudaiogawa/testbox/samp01.kt
@@ -0,0 +1,11 @@
+package com.yudaiogawa.testbox
+
+class Calc {
+    fun times(x: Int, y: Int): Int {
+        return x * y
+    }
+
+    fun divide(x: Int, y: Int): Double {
+        return x / y.toDouble()
+    }
+}
---
+++ kotlin/testbox/app/src/test/java/com/yudaiogawa/testbox/CalculatorTest.kt
@@ -0,0 +1,37 @@
+package com.yudaiogawa.testbox
+
+import org.junit.Test
+import org.assertj.core.api.Assertions.*
+
+internal class CalculatorTest {
+
+    private val sut = Calc()
+
+    @Test
+    fun times() {
+        val act: Int = sut.times(2, 3)
+        val exp = 6
+        assertThat(act).isEqualTo(exp)
+    }
+
+    @Test
+    fun times02() {
+        val act: Int = sut.times(0, 3)
+        val exp = 0
+        assertThat(act).isEqualTo(exp)
+    }
+
+    @Test
+    fun divide() {
+        val act: Double = sut.divide(6, 2)
+        val exp = 3.0
+        assertThat(act).isEqualTo(exp)
+    }
+
+    @Test
+    fun divide02() {
+        val act: Double = sut.divide(3, 2)
+        val exp = 1.5
+        assertThat(act).isEqualTo(exp)
+    }
+}
