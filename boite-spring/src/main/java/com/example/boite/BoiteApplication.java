f3513f5c099c927d99880addc435060ced3485a0
yudaiogawa <19630931+yudaiogawa@users.noreply.github.com>
2020-09-15 22:29:57 +0900

    add: boite spring

---
+++ boite-spring/src/main/java/com/example/boite/BoiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.boite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoiteApplication.class, args);
+    }
+
+}
---
+++ boite-spring/src/test/java/com/example/boite/BoiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
