491b3ce9f5135edc35b8dc5c7c93dd35ba0efb88
yudaiogawa <19630931+yudaiogawa@users.noreply.github.com>
2020-11-27 21:11:32 +0900

    add: talk-session app

--- boite_django/boite_django/settings.py
+++ boite_django/boite_django/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'talk_session',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ boite_django/talk_session/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TalkSessionConfig(AppConfig):
+    name = 'talk_session'
