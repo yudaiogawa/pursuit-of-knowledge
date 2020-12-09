4e911de11339504ddeb4d0a7c4c87baad442e1a3
yudaiogawa <19630931+yudaiogawa@users.noreply.github.com>
2020-12-09 23:42:37 +0900

    refactor: define path

--- boite_django/talk_session/urls.py
+++ boite_django/talk_session/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('sess/<str:sess_name>/', views.sess, name='sess'),
-    path('', views.index, name='index')
 ]
