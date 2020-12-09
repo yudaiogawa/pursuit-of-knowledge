5cea9c610a3a88793c6e3d170a9516b52e8a0928
yudaiogawa <19630931+yudaiogawa@users.noreply.github.com>
2020-12-09 23:34:21 +0900

    add: session room used websocket

--- boite_django/talk_session/urls.py
+++ boite_django/talk_session/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('sess/<str:sess_name>/', views.sess, name='sess'),
     path('', views.index, name='index')
 ]
--- boite_django/talk_session/views.py
+++ boite_django/talk_session/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(req):
     return render(req, 'talk_session/index.html')
+
+
+def sess(req, sess_name):
+    return render(req, 'talk_session/sess-room.html', {'sess_name': sess_name})
